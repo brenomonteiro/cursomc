090569ba9257ac6d090c5d675bb828bf99135641
brenomonteiro <brennoo.mendesmonteiro@gmail.com>
2018-09-13 20:41:17 -0300

    Paginação com parâmetros opicionais na requisição

--- src/main/java/com/aula/demo/CursomcApplication.java
+++ src/main/java/com/aula/demo/CursomcApplication.java
@@ -72,6 +72,11 @@ private ItemPedidoRepository itemPedidoRepository;
 		// TODO Auto-generated method stub
 		Categoria cat1 = new Categoria(null,"informática");
 		Categoria cat2 = new Categoria(null,"escritório");
+		Categoria cat3 = new Categoria(null,"cama mesa e banho");
+		Categoria cat4 = new Categoria(null,"Eletrônicos");
+		Categoria cat5 = new Categoria(null,"Jardinagem");
+		Categoria cat6 = new Categoria(null,"Decoração");
+		Categoria cat7 = new Categoria(null,"Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -80,12 +85,13 @@ private ItemPedidoRepository itemPedidoRepository;
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
+		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		
