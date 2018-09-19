d470895b654016623f52e34f82e223c4b1e897fb
brenomonteiro <brennoo.mendesmonteiro@gmail.com>
2018-09-19 18:42:22 -0300

    subtotal e total do pedido

--- src/main/java/com/aula/demo/domain/ItemPedido.java
+++ src/main/java/com/aula/demo/domain/ItemPedido.java
@@ -31,6 +31,11 @@ public class ItemPedido implements Serializable{
 		
 		
 	}
+	
+	public double getSubTotal() {
+		
+		return(preco - desconto)* quantidade;
+	}
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
