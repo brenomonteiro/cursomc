29ae2b8f332d62a16e332bb6f609b1c6cc9cff79
brenomonteiro <brennoo.mendesmonteiro@gmail.com>
2018-09-21 13:16:19 -0300

    Inserindo Pedido

--- src/main/java/com/aula/demo/domain/ItemPedido.java
+++ src/main/java/com/aula/demo/domain/ItemPedido.java
@@ -42,12 +42,20 @@ public class ItemPedido implements Serializable{
 		
 	}
 	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+		
+	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
 		
 	}
 	
-	
+	public void setProduto(Produto produto) {
+	 id.setProduto(produto);
+		
+	}
 
 	public ItemPedidoPK getId() {
 		return id;
--- src/main/java/com/aula/demo/resources/PedidoResources.java
+++ src/main/java/com/aula/demo/resources/PedidoResources.java
@@ -1,16 +1,22 @@
 package com.aula.demo.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.aula.demo.domain.Categoria;
 import com.aula.demo.domain.Pedido;
+import com.aula.demo.dto.CategoriaDTO;
 import com.aula.demo.services.PedidoService;
 
 @RestController
@@ -27,6 +33,16 @@ public class PedidoResources {
 				
 	
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.POST) 
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
